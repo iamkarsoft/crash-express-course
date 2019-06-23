55fc699c763e0b4b38c4cfcba67c74b2cf5bb642
Kofi Ramos Amoussou <karsoft92@gmail.com>
2019-06-23 16:09:27 +0000

    feat: delete member

--- routes/api/members.js
+++ routes/api/members.js
@@ -61,5 +61,20 @@ router.put('/:id', (req, res) => {
   }
 });
 
+// delete member
+router.delete('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    // res.send(req.params.id);
+    res.json({ msg: 'Member Deleted', 
+    members:members.filter(member => member.id !== parseInt(req.params.id))});
+  } else {
+    res.status(400).json({
+      msg: `No member with id of ${req.params.id}`
+    });
+  }
+});
+
 
 module.exports = router;
